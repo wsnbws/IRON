dc4b0efb82882b925e9a6752aa9e8d758cb631a1
ws_a100 <wangshuo24z@ict.ac.cn>
2025-10-22 17:18:48 +0800

    dinov3 adapt

--- backbone/__init__.py
+++ backbone/__init__.py
@@ -1,3 +1,3 @@
-from . import convmae,mae,video_convmae,static_convmae
+from . import convmae,mae,video_convmae,static_convmae, dino_3
 
-__all__ = ['convmae','mae','video_convmae','static_convmae']
+__all__ = ['convmae','mae','video_convmae','static_convmae','dino_3']
